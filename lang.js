3fdda1ade8e626180aa9810b5378a02b7de6cd37
Francois couillard <fgosselin@cofomo.com>
2018-05-02 16:27:10 -0400

    Added 2 other ways to translate: 1. without key id on page load 2. without key id after page load

--- lang.js
+++ lang.js
@@ -1,24 +1,55 @@
-var langs = ['en', 'it'];
-var langCode = '';
-var langJS = null;
-
-
-var translate = function (jsdata)
-{	
-	$("[tkey]").each (function (index)
-	{
-		var strTr = jsdata [$(this).attr ('tkey')];
-	    $(this).html (strTr);
-	});
+class Lang {
+	constructor () {
+		this.langs = ['fr', 'en', 'it'];
+		this.langBrowser = '';
+		this.library = {}; //every words of the good lang
+		this.langSet = '';
+		
+		if (navigator.language.substr (0, 2) != undefined) {
+			this.langBrowser = navigator.language.substr (0, 2);
+			if (this.langs.includes(this.langBrowser)) 
+				this.langSet = this.langBrowser;
+		}
+	};
+	loadAndSet () {
+		$.getJSON('lang/'+this.langSet+'.json', function (jsdata) {
+			this.library = jsdata;
+		
+			$("[tkey]").each (function (index) {
+				if ( $(this).attr ('tkey').length )
+					var strTr = jsdata [$(this).attr ('tkey')];
+				else {
+					var strTr = jsdata [$(this).html()];
+					$(this).attr( 'tkey', $(this).html() );
+				}
+				
+				$(this).attr("lang", $("html").attr("lang"));
+				$(this).html (strTr);
+			});
+		}.bind(this));
+	};
+	//Get a div with translated text with all infos for future translate
+	getTextContainer (str) {
+		return ( $("<div/>").attr("tkey", str).attr("lang", this.langSet).html(this.getText(str)) );
+	};
+	getText(key) {
+		if (this.library[key] == undefined)
+			console.log ("Clé introuvable=" + key);
+		
+		return (this.library[key] == undefined ? key : this.library[key] );
+	};
+	setLang (lang) {
+		if (lang == undefined)
+			lang = 'fr';
+		
+		//If we have a library with this language
+		if (this.langs.includes(lang)) {
+			$("html").attr("lang", lang);
+			this.langSet = lang;
+			this.loadAndSet();
+		}
+	};
 }
 
-
-langCode = navigator.language.substr (0, 2);
-
-if (langCode in langs)
-	$.getJSON('lang/'+langCode+'.json', translate);
-else
-	$.getJSON('lang/en.json', translate);
-
-
-
+var lang = new Lang();
+lang.setLang();
